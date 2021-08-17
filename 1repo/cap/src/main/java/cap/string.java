505954fa039099d2c7809cf773d3249c1660b16d
gmuralim <gmuralim@DIN66009703.corp.capgemini.com>
2021-08-17 12:43:37 +0530

    salescommit

---
+++ 1repo/cap/src/main/java/cap/string.java
@@ -0,0 +1,10 @@
+package cap;
+
+public class string {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello");
+	}
+}
+	
---
+++ cap/src/main/java/cap/slaesclass.java
@@ -0,0 +1,16 @@
+package cap;
+
+import java.nio.file.FileSystem;
+
+public class slaesclass {
+
+	public void display() {
+		int[] arr= {23,45,56,78,6};
+		for(int num:arr)
+			System.out.println(num);
+	}
+	public void print() {
+		System.out.println("print all array numbers:");
+		display();
+	}
+}
